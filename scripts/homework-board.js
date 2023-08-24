1eadfc55d6423560a04f84b9590356046edbbc3b
LemCAE <tengenzhe@outlook.com>
2023-08-24 21:14:23 +0800

    更新弹窗及部分元素样式

--- scripts/homework-board.js
+++ scripts/homework-board.js
@@ -7,3 +7,9 @@ function closePoP(){
     let popDiv = document.getElementById("edit-window");
     popDiv.style.display = "none";
 }
+
+function ShowContent(id1,id2){
+    var liList = document.getElementById('ch1')
+    var liText = liList[0].textContent
+    document.getElementById('ch1ip').value = liText;
+}
