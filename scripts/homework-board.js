1c69ad34f6c391b90310da082c07f9c0214d7632
LemCAE <tengenzhe@outlook.com>
2023-08-24 15:23:54 +0800

    更新弹出窗口

---
+++ scripts/homework-board.js
@@ -0,0 +1,9 @@
+function openedit(){
+    var popBox = document.getElementById("edit-window")
+    popBox.style.display = "block";
+}
+
+function closePop(){
+    let popDiv = document.getElementById("edit-window");
+    popDiv.style.display = "none";
+}
